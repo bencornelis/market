4f0830102ad06dd819f21810b1adb7a795848625
Ben Cornelis <bencornelis02@gmail.com>
2017-09-25 00:09:22 -0700

    refactor Printer

--- lib/printer.rb
+++ lib/printer.rb
@@ -4,27 +4,35 @@ class Printer
   end
 
   def print_register
-    table = collect_rows
-    [*headers, *table].each do |row|
-      left   = row[0].ljust(10, ' ')
-      center = row[1].ljust(13, ' ')
-      right  = row[2].rjust(6, ' ')
-      puts "#{left}#{center}#{right}"
-    end
-    puts '-'*29
-    puts "#{format_price(checkout.total_price)}".rjust(29, ' ')
+    [*headers, *collect_rows].each { |row| print_row(row) }
+    print_divider
+    print_total
   end
 
   private
 
   attr_reader :checkout
 
+  def print_row(row)
+    left   = row[0].ljust(10, ' ')
+    center = row[1].ljust(13, ' ')
+    right  = row[2].rjust(6, ' ')
+    puts "#{left}#{center}#{right}"
+  end
+
+  def print_divider
+    puts '-'*29
+  end
+
+  def print_total
+    puts "#{format_price(checkout.total_price)}".rjust(29, ' ')
+  end
+
   def collect_rows
     checkout.basket.each_with_object([]) do |item, rows|
       rows << [item.code, '', format_price(item.price)]
       next unless item.discounted?
-      discount = item.discount
-      rows << ['', discount.name, "-#{format_price(item.discount_amount)}"]
+      rows << ['', item.discount.name, "-#{format_price(item.discount_amount)}"]
     end
   end
 
