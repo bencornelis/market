8159072a3c6b678ede6a5c3236f6188b44c2e416
Ben Cornelis <bencornelis02@gmail.com>
2017-09-25 20:39:02 -0700

    organize lib using market file and directory

---
+++ lib/market.rb
@@ -0,0 +1,8 @@
+require_relative 'market/checkout'
+require_relative 'market/item'
+require_relative 'market/printer'
+require_relative 'market/discount'
+require_relative 'market/discounter'
+require_relative 'market/discounter/appl_discounter'
+require_relative 'market/discounter/bogo_discounter'
+require_relative 'market/discounter/chmk_discounter'
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,14 +1,6 @@
-require 'pry'
 require 'rspec'
 require 'support/checkout_helpers'
-require 'checkout'
-require 'item'
-require 'printer'
-require 'discount'
-require 'discounter'
-require 'discounter/appl_discounter'
-require 'discounter/bogo_discounter'
-require 'discounter/chmk_discounter'
+require 'market'
 
 RSpec.configure do |config|
   config.color = true
